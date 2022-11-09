6bf4c7e998afe6861c462768f635b82a0483407b
Ma Zl <yee@yee.bar>
2022-11-09 15:31:48 +0800

    1

--- mswebhook/index.js
+++ mswebhook/index.js
@@ -3,7 +3,6 @@ let data = require('@begin/data')
 const fetch = require('node-fetch')
 var jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '../.env' })
-let reftoken=require('./reftok.js')
 const { URLSearchParams } = require('url');
 const TelegramBot = require('node-telegram-bot-api');
 var escape = require('escape-html');
@@ -11,6 +10,45 @@ const util = require('util');
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TG_TOKEN
 const tgbot=new TelegramBot(token)
+
+async function reftoken(refresh_token){
+    //take refresh_token and refresh it ,returning the new a_token,and update it in the db
+    //db.key is jwt's sub
+    const params = new URLSearchParams();
+    params.append('client_id', process.env.CLIENT_ID);
+   // params.append('scope', 'Calendars.ReadWrite ChannelMessage.Read.All Mail.ReadWrite Mail.Send offline_access openid User.Read');
+   params.append('refresh_token', refresh_token);
+   // params.append('redirect_uri', process.env.REDIRECT_URI);
+    params.append('grant_type', 'refresh_token');
+    params.append('client_secret', process.env.CLIENT_SECRET);
+    
+    
+    res=await fetch('https://login.microsoftonline.com/'+process.env.TENANT_ID+'/oauth2/v2.0/token', { method: 'POST', body: params })
+    res=await res.json()
+    uid=jwt.decode(res.id_token).sub
+    console.log(uid)
+    a_token=res.access_token
+    r_token=res.refresh_token
+    expiry= Date.now()/1000+res.expires_in
+    res=await data.set({table:'users','key':uid,a_token:a_token,r_token:r_token,expiry:expiry})
+    console.log('==>',res)
+    return a_token
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 exports.handler = async function create(req) {
   let todo = arc.http.helpers.bodyParser(req)
   if('validationToken' in req.queryStringParameters){
@@ -26,7 +64,7 @@ exports.handler = async function create(req) {
     }
   }
   if(Date.now()/1000>=res.expiry){
-    utoken= await reftoken.refresh_token()
+    utoken= await reftoken()
   }else{
     utoken=res.a_token
   }
